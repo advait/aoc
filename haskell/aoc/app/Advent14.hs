a2a1e659dc2233438d781073b4cca90f77f92d62
Advait Shinde <advait.shinde@gmail.com>
2019-03-06 11:37:56 -0800

    Solve problem 14b.

--- haskell/aoc/app/Advent14.hs
+++ haskell/aoc/app/Advent14.hs
@@ -2,6 +2,7 @@ module Advent14 where
 
 import qualified Data.IntMap as IntMap
 import qualified Data.Maybe  as Maybe
+import qualified Debug.Trace as Trace
 
 type Recipe = Int
 
@@ -10,10 +11,10 @@ type Recipes = IntMap.IntMap Recipe
 type ElfPos = Int
 
 data State =
-  State Recipes
-        ElfPos
-        ElfPos
-        Int
+  State Recipes -- Recipes we have accumulated
+        ElfPos  -- Position of first elf
+        ElfPos  -- Position of second elf
+        Int     -- Length of Recipes
 
 -- Explode an Int by its digits
 explodeInt :: Int -> [Int]
@@ -23,12 +24,10 @@ explodeInt n = map charToInt (show n)
 
 -- Generate the next state from the current one
 step :: State -> State
-step (State rs e1 e2 nextIndex)
-  | rs `seq` e1 `seq` e2 `seq` False = undefined
 step (State rs e1 e2 nextIndex) = State newRs newE1 newE2 newNextIndex
   where
-    r1 = Maybe.fromJust $ IntMap.lookup e1 rs
-    r2 = Maybe.fromJust $ IntMap.lookup e2 rs
+    r1 = rs IntMap.! e1
+    r2 = rs IntMap.! e2
     newRecipes = explodeInt (r1 + r2)
     insertRecipe index recipe rs =
       let nextRs = IntMap.insert index recipe rs
@@ -39,22 +38,43 @@ step (State rs e1 e2 nextIndex) = State newRs newE1 newE2 newNextIndex
 
 startingState = State (IntMap.fromList [(0, 3), (1, 7)]) 0 1 2
 
+-- Generate at least n recipes, returning the final state
 genNRecipes :: Int -> State -> State
-genNRecipes n state@(State _ _ _ generatedRecipes) | n <= generatedRecipes = state
+genNRecipes n state@(State _ _ _ generatedRecipes)
+  | n <= generatedRecipes = state
 genNRecipes n state = genNRecipes n (step state)
 
+-- Returns whether the given candidate search matches at the given position
+candidateMatchesAt :: [Int] -> Int -> Recipes -> Bool
+candidateMatchesAt [] _ _ = True
+candidateMatchesAt (c:t) i rs
+  | (rs IntMap.! i) /= c = False
+candidateMatchesAt (_:t) i rs = candidateMatchesAt t (i + 1) rs
+
 -- Returns the 10 recipe scores after we've generated n recipes
-getRecipeScores n = showRecipes endRecipes
+solvePart1 n = showRecipes endRecipes
   where
     recipesToGen = n + 10
     (State recipes _ _ _) = genNRecipes recipesToGen startingState
-    endRecipes = map (\i -> Maybe.fromJust $ IntMap.lookup i recipes) [n..n+9]
+    endRecipes = map (recipes IntMap.!) [n .. n + 9]
+    showRecipes = map (head . show) -- Convert [Recipe] to String
 
-showRecipes :: [Recipe] -> String
-showRecipes = map (stringToChar . show)
+-- Generates recipes until we see the given recipes in a row, returning the index of the match
+genRecipesUntil :: Int -> Int -> State -> Int
+genRecipesUntil i = rec
   where
-    stringToChar [s] = s
-    stringToChar _ = error "Too many chars in string"
+    toFind = explodeInt i
+    toFindLen = length toFind -- Warning: This might not be 6 if we have preceding zeroes!
+    rec :: Int -> State -> Int
+    rec candidateIndex state@(State _ _ _ nextIndex) -- Not enough recipes generated to perform search
+      | (candidateIndex + toFindLen) > nextIndex = rec candidateIndex $ step state
+    rec candidateIndex state@(State rs _ _ nextIndex)
+      | candidateMatchesAt toFind candidateIndex rs = candidateIndex -- Found match
+      | otherwise = rec (candidateIndex + 1) state -- Did not find match
 
 main :: IO ()
-main = putStrLn (getRecipeScores 825401)
+main = do
+  let input = 825401
+  let p1 = solvePart1 input
+  let p2 = show $ genRecipesUntil input 0 startingState
+  putStrLn $ "Part 1: " ++ p1 ++ "\nPart 2: " ++ p2
