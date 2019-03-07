f7f8350425b69721cec1391eb44e9f6900aa4fe5
Advait Shinde <advait.shinde@gmail.com>
2019-03-07 12:14:40 -0800

    Implement comparePath.

--- haskell/aoc/app/Advent15.hs
+++ haskell/aoc/app/Advent15.hs
@@ -2,6 +2,7 @@ module Advent15 where
 
 -- Represents an abstract node for pathfinding algorithms
 class Ord a => Node a where
+  -- Returns neighbors in *sorted* order
   neighbors :: a -> [a]
 
 -- (X, Y) cartesian coordinates
@@ -14,6 +15,16 @@ instance Ord Pos where
 
 instance Show Pos where
   show (Pos x y) = show (x, y)
+
+-- Alternative compare function for [Node]/Paths that prefers shorter paths.
+comparePath :: (Node a) => [a] -> [a] -> Ordering
+comparePath [] [] = EQ
+comparePath a b
+  | lenA < lenB = LT
+  | lenA > lenB = GT
+  | otherwise = compare a b
+  where lenA = length a
+        lenB = length b
 
 main :: IO ()
 main = undefined
--- haskell/aoc/test/Advent15Spec.hs
+++ haskell/aoc/test/Advent15Spec.hs
@@ -1,6 +1,6 @@
 module Advent15Spec where
 
-import Advent15
+import           Advent15
 import           Data.Foldable
 import qualified Data.IntMap   as IntMap
 import           Data.List
@@ -9,11 +9,22 @@ import qualified Debug.Trace   as Trace
 import qualified Deque
 import           Test.Hspec
 
+instance Node Pos where
+  neighbors (Pos x y) = sort [Pos (x - 1) y, Pos (x + 1) y, Pos x (y - 1), Pos x (y + 1)]
+
 spec :: Spec
-spec = describe "Pos" $ do
-  it "Compares Y before X" $
-    compare (Pos 1 0) (Pos 0 1) `shouldBe` LT
-  it "Orders items on the same row" $
-    sort [Pos 1 0, Pos 0 0] `shouldBe` [Pos 0 0, Pos 1 0]
-  it "Orders y before x" $
-    sort [Pos 1 1, Pos 0 0, Pos 0 1, Pos 1 0] `shouldBe` [Pos 0 0, Pos 1 0, Pos 0 1, Pos 1 1]
+spec = do
+  describe "Pos" $ do
+    it "Compares Y before X" $ compare (Pos 1 0) (Pos 0 1) `shouldBe` LT
+    it "Orders items on the same row" $ sort [Pos 1 0, Pos 0 0] `shouldBe` [Pos 0 0, Pos 1 0]
+    it "Orders y before x" $ sort [Pos 1 1, Pos 0 0, Pos 0 1, Pos 1 0] `shouldBe` [Pos 0 0, Pos 1 0, Pos 0 1, Pos 1 1]
+    it "Selects the right neighbors" $ neighbors (Pos 0 0) `shouldBe` sort [Pos 0 1, Pos 0 (-1), Pos 1 0, Pos (-1) 0]
+  describe "[Node]/Path" $ do
+    it "Prefers shorter paths to the same destination" $ do
+      let p1 = [Pos 0 0, Pos 1 0, Pos 1 1]
+      let p2 = [Pos 0 0, Pos 0 1, Pos 0 2, Pos 1 2, Pos 1 1]
+      comparePath p1 p2 `shouldBe` LT
+    it "Prefers three-length paths according to reading order" $ do
+      let p1 = [Pos 0 0, Pos 1 0, Pos 1 1]
+      let p2 = [Pos 0 0, Pos 0 1, Pos 1 1]
+      comparePath p1 p2 `shouldBe` LT
