06752daeb329b431f79be1b1a68a8136416c2dca
Advait Shinde <advait.shinde@gmail.com>
2019-03-07 15:29:41 -0800

    Implement shortestPath.

--- haskell/aoc/app/Advent15.hs
+++ haskell/aoc/app/Advent15.hs
@@ -1,12 +1,20 @@
 module Advent15 where
 
+import qualified Data.Map    as Map
+import qualified Data.Set    as Set
+import qualified Debug.Trace as Trace
+
 -- Represents an abstract node for pathfinding algorithms
-class Ord a => Node a where
+class (Ord a, Show a) =>
+      Node a
   -- Returns neighbors in *sorted* order
+  where
   neighbors :: a -> [a]
 
 -- (X, Y) cartesian coordinates
-data Pos = Pos Int Int
+data Pos =
+  Pos Int
+      Int
   deriving (Eq)
 
 -- Order positions according to "reading order": y first then x.
@@ -16,15 +24,35 @@ instance Ord Pos where
 instance Show Pos where
   show (Pos x y) = show (x, y)
 
--- Alternative compare function for [Node]/Paths that prefers shorter paths.
+-- Alternative compare function for [Node]/Paths that prefers shorter paths
+-- before going into a node-by-node comparison if paths are equal length.
 comparePath :: (Node a) => [a] -> [a] -> Ordering
-comparePath [] [] = EQ
 comparePath a b
   | lenA < lenB = LT
   | lenA > lenB = GT
   | otherwise = compare a b
-  where lenA = length a
-        lenB = length b
+  where
+    lenA = length a
+    lenB = length b
+
+-- Returns the shortest path from the first node to the second or Nothing
+-- if we have exhaustively decided that no such path exists.
+shortestPath :: (Node a) => a -> a -> Maybe [a]
+shortestPath src dst = searchPathQueue dst Set.empty startQueue
+  where
+    startPath = [src] -- We build up a Path from the src to the dst, node-by-node
+    startQueue = [startPath] -- We work from a queue of Paths
+    searchPathQueue :: (Node a) => a -> Set.Set a -> [[a]] -> Maybe [a]
+    searchPathQueue _ _ [] = Nothing -- No more paths left in the queue, exhausted search space
+    searchPathQueue dst seen (curPath:queue)
+      | curNode == dst = Just curPath -- Reached our destination using curPath
+      | otherwise = searchPathQueue dst newSeen newQueue
+      where
+        curNode = last curPath
+        elligibleNeighbors = filter (`Set.notMember` seen) $ neighbors curNode
+        newPaths = map (\n -> curPath ++ [n]) elligibleNeighbors
+        newQueue = queue ++ newPaths
+        newSeen = Set.insert curNode seen
 
 main :: IO ()
 main = undefined
--- haskell/aoc/test/Advent15Spec.hs
+++ haskell/aoc/test/Advent15Spec.hs
@@ -1,25 +1,22 @@
 module Advent15Spec where
 
 import           Advent15
-import           Data.Foldable
-import qualified Data.IntMap   as IntMap
 import           Data.List
-import qualified Data.Maybe    as Maybe
-import qualified Debug.Trace   as Trace
-import qualified Deque
+import qualified Data.Maybe  as Maybe
+import qualified Data.Set    as Set
+import qualified Debug.Trace as Trace
 import           Test.Hspec
-
-instance Node Pos where
-  neighbors (Pos x y) = sort [Pos (x - 1) y, Pos (x + 1) y, Pos x (y - 1), Pos x (y + 1)]
 
 spec :: Spec
 spec = do
   describe "Pos" $ do
     it "Compares Y before X" $ compare (Pos 1 0) (Pos 0 1) `shouldBe` LT
     it "Orders items on the same row" $ sort [Pos 1 0, Pos 0 0] `shouldBe` [Pos 0 0, Pos 1 0]
-    it "Orders y before x" $ sort [Pos 1 1, Pos 0 0, Pos 0 1, Pos 1 0] `shouldBe` [Pos 0 0, Pos 1 0, Pos 0 1, Pos 1 1]
-    it "Selects the right neighbors" $ neighbors (Pos 0 0) `shouldBe` sort [Pos 0 1, Pos 0 (-1), Pos 1 0, Pos (-1) 0]
-  describe "[Node]/Path" $ do
+    it "Orders Y before X in sort" $
+      sort [Pos 1 1, Pos 0 0, Pos 0 1, Pos 1 0] `shouldBe` [Pos 0 0, Pos 1 0, Pos 0 1, Pos 1 1]
+    it "Selects adjacent neighbors in sorted order" $
+      neighbors (Pos 0 0) `shouldBe` sort [Pos 0 1, Pos 0 (-1), Pos 1 0, Pos (-1) 0]
+  describe "comparePath" $ do
     it "Prefers shorter paths to the same destination" $ do
       let p1 = [Pos 0 0, Pos 1 0, Pos 1 1]
       let p2 = [Pos 0 0, Pos 0 1, Pos 0 2, Pos 1 2, Pos 1 1]
@@ -28,3 +25,30 @@ spec = do
       let p1 = [Pos 0 0, Pos 1 0, Pos 1 1]
       let p2 = [Pos 0 0, Pos 0 1, Pos 1 1]
       comparePath p1 p2 `shouldBe` LT
+  describe "Set Path" $
+    it "Detects inserted elements properly" $ do
+      let s1 = Set.insert (Pos 0 0) Set.empty
+      Set.member (Pos 0 0) s1 `shouldBe` True
+      Set.member (Pos 0 1) s1 `shouldBe` False
+      let s2 = Set.fromList $ map (Pos 0) [0 .. 100]
+      Set.member (Pos 0 69) s2 `shouldBe` True
+  describe "shortestPath" $ do
+    it "Returns a one-length path when the src == dst" $ shortestPath (Pos 0 0) (Pos 0 0) `shouldBe` Just [Pos 0 0]
+    it "Returns a two-length path when dst is a neighbor of src" $ do
+      shortestPath (Pos 0 0) (Pos 0 1) `shouldBe` Just [Pos 0 0, Pos 0 1]
+      shortestPath (Pos 0 0) (Pos 1 0) `shouldBe` Just [Pos 0 0, Pos 1 0]
+      shortestPath (Pos 0 0) (Pos 0 (-1)) `shouldBe` Just [Pos 0 0, Pos 0 (-1)]
+      shortestPath (Pos 0 0) (Pos (-1) 0) `shouldBe` Just [Pos 0 0, Pos (-1) 0]
+    it "Returns the reading-ordering three-length path appropriately" $ do
+      shortestPath (Pos 0 0) (Pos 1 1) `shouldBe` Just [Pos 0 0, Pos 1 0, Pos 1 1]
+      shortestPath (Pos 0 0) (Pos (-1) 1) `shouldBe` Just [Pos 0 0, Pos (-1) 0, Pos (-1) 1]
+      shortestPath (Pos 0 0) (Pos 1 (-1)) `shouldBe` Just [Pos 0 0, Pos 0 (-1), Pos 1 (-1)]
+      shortestPath (Pos 0 0) (Pos (-1) (-1)) `shouldBe` Just [Pos 0 0, Pos 0 (-1), Pos (-1) (-1)]
+    it "Returns a path of length (n+1) if dst is n units away from the origin" $ do
+      let n = 10
+      let path = map (Pos 0) [0 .. n]
+      shortestPath (Pos 0 0) (Pos 0 n) `shouldBe` Just path
+
+-- As a test, allow Pos to implement the Node class
+instance Node Pos where
+  neighbors (Pos x y) = sort [Pos (x - 1) y, Pos (x + 1) y, Pos x (y - 1), Pos x (y + 1)]
