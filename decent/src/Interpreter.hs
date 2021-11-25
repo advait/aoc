56259ad26de6f51a1d5cb3bb036667559e50e6bd
Advait Shinde <advait.shinde@gmail.com>
2021-11-25 12:58:39 -0800

    Implement def!.

--- decent/src/Interpreter.hs
+++ decent/src/Interpreter.hs
@@ -25,11 +25,13 @@ data ErrorType
   = ArgumentCountError Int Int
   | ReferenceError
   | TypeError DType DType
+  | EmptyStackError
 
 instance Show ErrorType where
   show (ArgumentCountError expected actual) = "ArgumentCountError (expected: " <> show expected <> ", actual: " <> show actual <> ")"
   show ReferenceError = "ReferenceError"
   show (TypeError expected actual) = "TypeError (expected: " <> show expected <> ", actual: " <> show actual <> ")"
+  show EmptyStackError = "EmptyStackError (no stack frames for binding new value)"
 
 -- | Evaluates the given expression, yielding a value.
 eval :: DExpr -> Interpreter DExpr
@@ -37,7 +39,15 @@ eval :: DExpr -> Interpreter DExpr
 eval i@(DInt _) = pure i
 -- Empty lists evaluate to themselves
 eval i@(DList []) = pure i
--- Function calls
+-- Special form function calls
+-- def! binds teh name to the given value
+eval e@(DList (DSymbol "def!" : params)) = do
+  (p1, p2) <- expect2 params
+  name <- expectSymbol p1
+  value <- eval p2
+  _ <- setBinding name value
+  pure value
+-- General function calls
 eval e@(DList (name : params)) = do
   binding <- lookup name
   case binding of
@@ -85,6 +95,16 @@ lookup expr = do
         Just binding -> pure binding
   lookup' $ envStack state
 
+-- | Sets the key to the given value in the current environment stack.
+setBinding :: String -> DExpr -> Interpreter ()
+setBinding key value = do
+  let putBinding :: [Bindings] -> Interpreter [Bindings]
+      putBinding [] = iError EmptyStackError value
+      putBinding (head : tail) = pure $ Map.insert key (StaticBinding value) head : tail
+  state <- State.get
+  envStack' <- putBinding (envStack state)
+  State.put $ state {envStack = envStack'}
+
 -- | Helper to construct and throw errors.
 iError :: ErrorType -> DExpr -> Interpreter a
 iError errorType expr = lift $ throwE $ IError errorType expr
@@ -96,6 +116,15 @@ expectSymbol e = iError (TypeError TSymbol (typeOf e)) e
 expectInt :: DExpr -> Interpreter Int
 expectInt (DInt i) = pure i
 expectInt e = iError (TypeError TInt (typeOf e)) e
+
+expect2 :: [a] -> Interpreter (a, a)
+expect2 [p1, p2] = pure (p1, p2)
+expect2 l = iError (ArgumentCountError 2 (length l)) undefined
+
+expectFn2 :: DExpr -> Interpreter (DExpr, DExpr)
+expectFn2 (DList [p1, p2]) = pure (p1, p2)
+expectFn2 e@(DList l) = iError (ArgumentCountError 2 (length l)) e
+expectFn2 e = iError (TypeError TList (typeOf e)) e
 
 fn2IntIntInt :: (Int -> Int -> Int) -> [DExpr] -> Interpreter DExpr
 fn2IntIntInt f [DInt p1, DInt p2] = pure $ DInt $ f p1 p2
