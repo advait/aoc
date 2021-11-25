31a884f568c84145782f4d2f05ce194328c4ed5c
Advait Shinde <advait.shinde@gmail.com>
2021-11-25 11:03:17 -0800

    Begin support for recursive environemnts.

--- decent/src/Interpreter.hs
+++ decent/src/Interpreter.hs
@@ -3,6 +3,9 @@ module Interpreter where
 import Control.Monad.Trans.Class (lift)
 import Control.Monad.Trans.Except (Except, runExcept, throwE)
 import Control.Monad.Trans.State (StateT (runStateT), evalStateT)
+import qualified Control.Monad.Trans.State as State
+import Data.Map (Map)
+import qualified Data.Map as Map
 import GHC.Base (divInt)
 import Types
 import Prelude hiding (error, lookup)
@@ -11,7 +14,7 @@ import Prelude hiding (error, lookup)
 -- | evaluates to a value v.
 type Interpreter v = StateT IState (Except IError) v
 
-type IState = ()
+newtype IState = IState {envStack :: [Bindings]}
 
 data IError = IError ErrorType DExpr
 
@@ -42,32 +45,49 @@ eval e@(DList (name : params)) = do
     BuiltinFn f -> do
       evalParams <- sequence (eval <$> params)
       f evalParams
+-- Dereference symbols relative to the environment
 eval i@(DSymbol s) = do
   binding <- lookup i
   case binding of
     StaticBinding b -> pure b
     BuiltinFn _ -> iError ReferenceError i
 
-iError :: ErrorType -> DExpr -> Interpreter a
-iError errorType expr = lift $ throwE $ IError errorType expr
-
 data Binding
   = StaticBinding DExpr
   | BuiltinFn ([DExpr] -> Interpreter DExpr)
 
--- | Looks up a name in the environment.
-lookup' :: String -> Maybe Binding
-lookup' "+" = Just $ BuiltinFn $ fn2IntIntInt (+)
-lookup' "-" = Just $ BuiltinFn $ fn2IntIntInt (-)
-lookup' "*" = Just $ BuiltinFn $ fn2IntIntInt (*)
-lookup' "/" = Just $ BuiltinFn $ fn2IntIntInt divInt
-lookup' _ = Nothing
+type Bindings = Map String Binding
 
+rootBindings :: Bindings
+rootBindings =
+  Map.fromList
+    [ ("+", BuiltinFn $ fn2IntIntInt (+)),
+      ("-", BuiltinFn $ fn2IntIntInt (-)),
+      ("*", BuiltinFn $ fn2IntIntInt (*)),
+      ("/", BuiltinFn $ fn2IntIntInt divInt)
+    ]
+
+initState :: IState
+initState =
+  IState
+    { envStack = [rootBindings]
+    }
+
+-- | Recursively looks up a value in the environment stack.
 lookup :: DExpr -> Interpreter Binding
-lookup e@(DSymbol name) = case lookup' name of
-  Just b -> pure b
-  Nothing -> iError ReferenceError e
-lookup e = iError (TypeError TSymbol (typeOf e)) e
+lookup expr = do
+  name <- expectSymbol expr
+  state <- State.get
+  let lookup' :: [Bindings] -> Interpreter Binding
+      lookup' [] = iError ReferenceError expr
+      lookup' (head : tail) = case Map.lookup name head of
+        Nothing -> lookup' tail
+        Just binding -> pure binding
+  lookup' $ envStack state
+
+-- | Helper to construct and throw errors.
+iError :: ErrorType -> DExpr -> Interpreter a
+iError errorType expr = lift $ throwE $ IError errorType expr
 
 expectSymbol :: DExpr -> Interpreter String
 expectSymbol (DSymbol s) = pure s
@@ -80,9 +100,6 @@ expectInt e = iError (TypeError TInt (typeOf e)) e
 fn2IntIntInt :: (Int -> Int -> Int) -> [DExpr] -> Interpreter DExpr
 fn2IntIntInt f [DInt p1, DInt p2] = pure $ DInt $ f p1 p2
 fn2IntIntInt _ _ = undefined
-
-initState :: IState
-initState = ()
 
 execInterpreter :: Interpreter DExpr -> IState -> Either IError (DExpr, IState)
 execInterpreter interp state = runExcept $ runStateT interp state
